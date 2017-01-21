d3a77e7abc5ca14c398e08c2278d04f71fa4ce4e
Paradoxyde <mr.vincent.couture@gmail.com>
2017-01-21 01:30:54 -0500

    Wall jumps, bitches!
    Wall jumps.

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -7,46 +7,79 @@ public class MainCharacterController : MonoBehaviour
 {
     public float max_speed = 12.0f;
     public float ground_check_radius = 0.05f;
-    public Transform feet_pos;
+    public float wall_check_radius = 0.15f;
+    public Transform feet_sensor_pos;
+    public Transform left_sensor_pos;
+    public Transform right_sensor_pos;
     public LayerMask ground_check_layers;
     public float jump_force = 1000.0f;
     public float high_jump_force = 2400.0f;
     public float high_jump_duration = 0.25f;
-    public float double_jump_force = 1000.0f;
+    public float double_jump_force = 850.0f;
     public float double_high_jump_force = 2400.0f;
     public float double_high_jump_duration = 0.25f;
-    public float air_control_force = 5000.0f;
-    public int continuous_jumps = 2;
-    public float test = 100f;
+    public float air_control_force = 6000.0f;
+    public int air_jumps = 2;
+    public float double_jump_vel_force_factor = 45f;
+    public float wall_hug_gravity_modifier = 0.25f;
+    public float wall_jump_horiz_force = 500.0f;
+    public float wall_jump_control_lock_duration = 0.25f;
+    public bool enable_wall_jump = true;
+    public bool wall_jump_refreshes_air_jump = true;
 
     Vector2 m_moveInput = new Vector2();
     bool m_jumpHeld = false;
     bool m_isFreshJumpPress = true;
     float m_timeSinceJumpPress = 0.0f;
     float m_timeSinceJumpStart = 0.0f;
-    bool m_isGrounded = false;
+    float m_timeHuggingWall = 0.0f;
     bool m_isJumping = false;
-    int m_continuousJumpCount = 0;
+    int m_airJumpCount = 0;
+    bool m_isHuggingWall = false;
+    bool m_wallHugIsOnRight = false;
+    float m_baseGravityScale = 10.0f;
+
+    bool m_isGrounded = false;
+    bool m_hasLeftContact = false;
+    bool m_hasRightContact = false;
 
     Rigidbody2D m_rigidBody;
+    JumpType m_jumpType = JumpType.first;
     
+    enum JumpType
+    {
+        first,
+        air,
+        wall
+    }
+
 	void Start()
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
-	}
+        m_baseGravityScale = m_rigidBody.gravityScale;
+
+    }
 	
 	void Update()
     {
+        UpdateContacts();
         UpdateInputs();
         UpdateHorizontalVelocity();
+        UpdateWallHugging();
         UpdateJumping();
+    }
+
+    void UpdateContacts()
+    {
+        m_isGrounded = Physics2D.OverlapCircle(feet_sensor_pos.position, ground_check_radius, ground_check_layers);
+        m_hasLeftContact = Physics2D.OverlapCircle(left_sensor_pos.position, wall_check_radius, ground_check_layers);
+        m_hasRightContact = Physics2D.OverlapCircle(right_sensor_pos.position, wall_check_radius, ground_check_layers);
     }
 
     void UpdateInputs()
     {
-        m_isGrounded = Physics2D.OverlapCircle(feet_pos.position, ground_check_radius, ground_check_layers);
         m_moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
+        
         bool jumpHeld = Input.GetButton("Jump");
         if (jumpHeld)
         {
@@ -77,12 +110,41 @@ public class MainCharacterController : MonoBehaviour
         }
         else
         {
-            if (currentVel.x >= -max_speed && m_moveInput.x < 0
-             || currentVel.x <= max_speed  && m_moveInput.x > 0)
+            if (!(m_isJumping && m_jumpType == JumpType.wall && m_timeSinceJumpStart < wall_jump_control_lock_duration))
             {
-                m_rigidBody.AddForce(new Vector2(m_moveInput.x * air_control_force * Time.deltaTime, 0.0f));
+                if ((currentVel.x >= -max_speed && m_moveInput.x < 0 && !m_hasLeftContact)
+                 || (currentVel.x <= max_speed && m_moveInput.x > 0 && !m_hasRightContact))
+                {
+                    m_rigidBody.AddForce(new Vector2(m_moveInput.x * air_control_force * Time.deltaTime, 0.0f));
+                }
             }
         }
+    }
+
+    void UpdateWallHugging()
+    {
+        Vector2 currentVel = m_rigidBody.velocity;
+        bool shouldHugWall = m_isHuggingWall;
+        if (!m_isHuggingWall)
+        {
+            shouldHugWall = (m_hasLeftContact && m_moveInput.x < -0.5f)
+             || (m_hasRightContact && m_moveInput.x > 0.5f);
+            shouldHugWall &= !m_isGrounded;
+            if (shouldHugWall) m_wallHugIsOnRight = m_hasRightContact;
+        }
+        else
+        {
+            shouldHugWall = (m_hasLeftContact || m_hasRightContact) && !m_isGrounded;
+            m_timeHuggingWall += Time.deltaTime;
+        }
+
+        if (m_isHuggingWall != shouldHugWall)
+        {
+            m_isHuggingWall = shouldHugWall;
+            if (m_isHuggingWall) m_timeHuggingWall = 0.0f;
+        }
+        
+        m_rigidBody.gravityScale = m_baseGravityScale * (m_isHuggingWall && currentVel.y <= 0 ? wall_hug_gravity_modifier : 1.0f);
     }
 
     void UpdateJumping()
@@ -109,30 +171,45 @@ public class MainCharacterController : MonoBehaviour
                     m_isFreshJumpPress = false;
                     m_timeSinceJumpStart = 0.0f;
                     m_isJumping = true;
-                    m_continuousJumpCount = 1;
+                    m_airJumpCount = 1;
+                    m_jumpType = JumpType.first;
                 }
-                else if (m_continuousJumpCount < continuous_jumps && currentVertSpeed <= 0.0f)
+                else if (enable_wall_jump && m_isHuggingWall && currentVertSpeed <= 0.0f && m_timeHuggingWall > 0.1f)
                 {
-                    // Double jump
-                    m_rigidBody.AddForce(new Vector2(0.0f, double_jump_force - currentVertSpeed * test));
+                    // Air jump
+                    m_rigidBody.AddForce(new Vector2(wall_jump_horiz_force * (m_wallHugIsOnRight ? -1.0f : 1.0f), double_jump_force - currentVertSpeed * double_jump_vel_force_factor));
                     m_isFreshJumpPress = false;
                     m_timeSinceJumpStart = 0.0f;
-                    m_continuousJumpCount++;
+                    m_jumpType = JumpType.wall;
+                    
+                    if (wall_jump_refreshes_air_jump)
+                    {
+                        m_airJumpCount = 1;
+                    }
+                }
+                else if (!m_isHuggingWall && m_airJumpCount < air_jumps && currentVertSpeed <= 0.0f)
+                {
+                    // Double jump
+                    m_rigidBody.AddForce(new Vector2(0.0f, double_jump_force - currentVertSpeed * double_jump_vel_force_factor));
+                    m_isFreshJumpPress = false;
+                    m_timeSinceJumpStart = 0.0f;
+                    m_airJumpCount++;
+                    m_jumpType = JumpType.air;
                 }
             }
             else
             {
                 // Handle different jump heights
-                if (m_isJumping)
+                if (m_isJumping && !m_hasLeftContact && !m_hasRightContact)
                 {
-                    if (m_continuousJumpCount == 1)
+                    if (m_jumpType == JumpType.first)
                     {
                         if (m_timeSinceJumpStart < high_jump_duration)
                         {
                             m_rigidBody.AddForce(new Vector2(0.0f, high_jump_force * Time.deltaTime));
                         }
                     }
-                    else
+                    else if (m_jumpType == JumpType.air || m_jumpType == JumpType.wall)
                     {
                         if (m_timeSinceJumpStart < double_high_jump_duration)
                         {
@@ -147,9 +224,13 @@ public class MainCharacterController : MonoBehaviour
     private void OnGUI()
     {
         GUI.color = Color.red;
-        GUI.Label(new Rect(20, 20, 200, 100), String.Format("Move input  : {0:0.00}:{1:0.00}", m_moveInput.x, m_moveInput.y));
-        GUI.Label(new Rect(20, 40, 200, 100),               "Jump held   : " + (m_jumpHeld ? "true" : "false"));
-        GUI.Label(new Rect(20, 60, 200, 100),               "Is grounded : " + (m_isGrounded ? "true" : "false"));
-        GUI.Label(new Rect(20, 80, 200, 100),               "Jump count : " + m_continuousJumpCount);
+        string contacts = "";
+        if (m_isGrounded) contacts += "ground ";
+        if (m_hasLeftContact) contacts += "left ";
+        if (m_hasRightContact) contacts += "right ";
+        GUI.Label(new Rect(20, 20, 200, 100), String.Format("Horiz input: {0:0.00}", m_moveInput.x));
+        GUI.Label(new Rect(20, 40, 200, 100),               "Jump held: " + (m_jumpHeld ? "true" : "false"));
+        GUI.Label(new Rect(20, 60, 200, 100),               "Hugging wall: " + (m_isHuggingWall ? "true" : "false"));
+        GUI.Label(new Rect(20, 80, 200, 100),               "Contacts : " + contacts);
     }
 }
