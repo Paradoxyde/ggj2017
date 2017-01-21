cfb7231501824b69d3dc257dd574fe47bd408aa6
xearus <francis.gagnon.globe@gmail.com>
2017-01-21 18:21:07 -0500

    Non francis!

---
+++ Assets/Scripts/sfx/ManagedClip.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ManagedClip
+{
+    public AudioClip AudioClip;
+    public float Volume = 0.5f;
+}
---
+++ Assets/Scripts/sfx/SFXManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+enum SFXPresetType
+{
+    Ambiance,
+    SFXPresetCount
+}
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioSource m_AudioSource0;
+    public AudioSource m_AusioSource1;
+
+    public SFXPreset m_PlayingPreset0;
+    public SFXPreset m_PlayingPreset1;
+
+    public ManagedClip m_PlayingClip0;
+    public ManagedClip m_PlayingClip1;
+
+    public SFXPreset[] m_SFXPresets = new SFXPresetCount[SFXPresetType.SFXPresetCount];
+
+    private bool IsMuted = false;
+    public float SFXMasterVolume = 1f;
+
+    public static SFXManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Debug.Assert(!Instance);
+
+        if (Instance != null && Instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (m_AudioSource0.isPlaying)
+
+        foreach (var preset in SFXPresets)
+            preset.Update();
+    }
+    
+    private float GetDampenerModifier(SoundDampener dampener)
+    {
+        return dampener != null ? dampener.getNextVolume() : 1.0f;
+    }
+}
