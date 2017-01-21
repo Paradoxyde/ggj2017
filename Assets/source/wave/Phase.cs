f83428e09c0b4b0177efa5004e756ab0f3472667
xearus <francis.gagnon.globe@gmail.com>
2017-01-21 04:36:27 -0500

    added wave form! and a couple modifications on the phasing system.

--- Assets/source/wave/Phase.cs
+++ Assets/source/wave/Phase.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class Phase
 {
+    public float PeriodInSeconds = 5.0f;
 
 }
--- Assets/source/wave/WaveComponent.cs
+++ Assets/source/wave/WaveComponent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WaveComponent : MonoBehaviour
 {
+
     void Start()
     {
         WaveManager.Instance.Register(this);
