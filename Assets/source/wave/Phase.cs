8f0cd1c83acc7e9eca212d6d8f4159982e27f0b6
Paradoxyde <mr.vincent.couture@gmail.com>
2017-01-21 04:28:06 -0500

    Added air jump hook
    also they work with the polarity system

--- Assets/source/wave/Phase.cs
+++ Assets/source/wave/Phase.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PhaseType
+{
+    Red,
+    Blue,
+    Neutral
+}
+
 [System.Serializable]
 public class Phase
 {
 
+    public PhaseType phase_type;
 }
