9c05ed2b635f0443b3edb9a9cab6ccc510af59e2
Monzerol <emery@monzerol.com>
2017-01-22 10:45:21 -0500

    * SineWave feedback * Tempo Scaler for tighter feedback * New BGM object for playing music to simplify handling of transitions

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -1,51 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BGM : TakuBehaviour
+public class BGM : MonoBehaviour
 {
 
-    public AudioClip intro;
-    public AudioClip core;
+    public static BGM Instance { get; private set; }
+
+    public AudioClip introMenu;
+    public AudioClip coreMenu;
+    public AudioClip introGame;
+    public AudioClip coreGame;
     public float fadeSpeed = 0.08f;
 
-    private AudioSource _introSource;
-    private AudioSource _coreSource;
+    public float tempo = 110f;
+    public float Time { get { return _gameSource.time; } }
+
+    private AudioSource _menuSource;
+    private AudioSource _gameSource;
+
+    public bool IsInGame = true;
 
     private void Start()
     {
+        Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        _coreSource = gameObject.AddComponent<AudioSource>();
-        _coreSource.clip = core;
+        _menuSource = gameObject.AddComponent<AudioSource>();
+        _menuSource.clip = introMenu;
+        _menuSource.loop = false;
+        _menuSource.Play();
+        _menuSource.volume = IsInGame ? 0f : 1f;
 
         GameObject introObj = new GameObject();
         introObj.transform.parent = transform;
-        _introSource = introObj.AddComponent<AudioSource>();
+        _gameSource = introObj.AddComponent<AudioSource>();
 
-        _introSource.clip = intro;
-        _introSource.loop = true;
-        _introSource.Play();
+        _gameSource.clip = introGame;
+        _gameSource.loop = false;
+        _gameSource.Play();
+        _menuSource.volume = IsInGame ? 1f : 0f;
     }
 
     private void Update()
     {
-        if (_coreSource.isPlaying)
+        // Intro/Loop transitions
+        if (!_menuSource.isPlaying)
         {
-            _coreSource.volume = Mathf.Min(_coreSource.volume + fadeSpeed, 1f);
-            _introSource.volume -= fadeSpeed;
+            _menuSource.clip = coreMenu;
+            _menuSource.loop = true;
+            _menuSource.Play();
+        }
+
+        if (!_gameSource.isPlaying)
+        {
+            _gameSource.clip = coreGame;
+            _gameSource.loop = true;
+            _gameSource.Play();
+        }
+
+
+        // Crossfades
+        if (IsInGame)
+        {
+            CrossFade(_menuSource, _gameSource);
+        }
+        else
+        {
+            CrossFade(_gameSource, _menuSource);
         }
     }
-
-    public void ActivateTransition()
+    
+    private void CrossFade(AudioSource from, AudioSource to)
     {
-        if (!_coreSource.isPlaying)
-        {
-            float block = intro.length / 4f;
-            _coreSource.Play();
-            _coreSource.time = _introSource.time % block;
-            _coreSource.volume = 0;
-            _coreSource.loop = true;
-        }
+        float maxVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+        to.volume = Mathf.Min(to.volume + fadeSpeed, maxVolume);
+        from.volume = Mathf.Max(from.volume - fadeSpeed, 0f);
     }
 
 }
